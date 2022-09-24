18c7a1151a28e84300cf7df051075416fcdef927
rongcl9219 <963941583@qq.com>
2022-09-24 21:12:04 +0800

    feat: 修改打包配置

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const CracoLessPlugin = require('craco-less');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
@@ -9,14 +11,34 @@ const addPath = dir => path.join(__dirname, dir);
 let pluginsArr = [];
 
 if (IS_PROD) {
-    pluginsArr.push(new CompressionWebpackPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: productionGzipExtensions,
-        threshold: 10240, // 对超过10k的数据进行压缩
-        minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
-        deleteOriginalAssets: false // 删除原文件
-    }));
+    pluginsArr.push(
+        new CompressionWebpackPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: productionGzipExtensions,
+            threshold: 10240, // 对超过10k的数据进行压缩
+            minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+            deleteOriginalAssets: false // 删除原文件
+        }),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                compress: {
+                    warnings: false,
+                    drop_debugger: true,
+                    drop_console: true
+                }
+            },
+            sourceMap: false,
+            parallel: true
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            analyzerHost: '127.0.0.1',
+            analyzerPort: 8888,
+            openAnalyzer: true, // 构建完关上浏览器
+            reportFilename: path.resolve(__dirname, 'analyzer/index.html')
+        })
+    );
 }
 
 module.exports = {
@@ -28,7 +50,6 @@ module.exports = {
         plugins: pluginsArr,
         configure: (config) => {
             let newConfig = config;
-
             newConfig.module.rules[1].oneOf = [
                 ...[
                     {
